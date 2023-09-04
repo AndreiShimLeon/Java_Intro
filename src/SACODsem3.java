eb10e847fdecf40b20f53b28cd20172093300cf5
AndreiShimLeon <art.shim@yandex.ru>
2023-09-04 16:22:28 +0300

    'Seminar 3 SACOD finished'

---
+++ src/SACODsem3.java
@@ -0,0 +1,190 @@
+class sList{
+    Node head;
+    class Node {
+        int value; // значение узла
+        Node next; // ссылка на следующий узел
+    }
+
+    void push_front(int value){ //вставка в начало (самое простое)
+        Node node = new Node();
+        node.value = value;
+        node.next = head;
+        head = node;
+    }
+
+    void push_back(int value){ //вставка в начало (самое простое)
+        Node node = new Node();
+        node.value = value;
+        if(head == null){
+            head = node;
+        }else{
+            Node current = head;
+            while(current.next != null){
+                current = current.next;
+            }
+            current.next = node;
+    }}
+
+    void pop_front(){ //методу удаления параметры не нужны
+        if (head != null) // делаем проверку того, что голова есть, а значит есть и ссылка на след узел у головы
+            head = head.next;
+    }
+
+    void pop_back(){ //методу удаления параметры не нужны
+        Node current = head;
+        if (head != null) { // делаем проверку того, что голова есть, а значит есть и ссылка на след узел у головы
+            if(head.next == null) head = null; // если в списке только 1 узел
+            else {
+                while (current.next.next != null) current = current.next; // если в списке 2 узла и более
+                current.next = null;
+            }
+        }
+    }
+
+    boolean contains(int value){
+        Node current = head;
+        while (current != null){
+            if (current.value == value) return true;
+            current = current.next;
+        }
+        return false;
+    }
+
+    void print(){
+        Node current = head;
+        while(current != null){
+            System.out.println(current.value);
+            current = current.next;
+        }
+    }
+
+
+}
+
+class dList{
+    Node head;
+    Node tail;
+    class Node {
+        int value; // значение узла
+        Node next; // ссылка на следующий узел
+        Node previous;  //  ссылка на предыдущий
+    }
+
+    void push_front(int value){ //вставка в начало (самое простое)
+        Node node = new Node();
+        node.value = value;
+        if(head == null){
+            tail = node;
+        } else{
+            node.next = head;
+            head.previous = node;
+        }
+        head = node;
+    }
+
+    void push_back(int value) { //вставка в конец как вставка в начало, только все ссылки на обратные
+        Node node = new Node();
+        node.value = value;
+        if (tail == null) {
+            head = node;
+        } else {
+            node.previous = tail;
+            tail.next = node;
+        }
+        tail = node;
+    }
+    void pop_front(){ //методу удаления параметры не нужны
+        if (head != null) // делаем проверку того, что голова есть, а значит есть и ссылка на след узел у головы
+            if(head.next == null) {
+                tail = null;
+                head = null;
+            } else {
+                head = head.next;
+                head.previous = null;
+            }
+    }
+
+    void pop_back(){ // меняем ссылки у pop front на обратные
+        if (tail != null)
+            if(tail.previous == null) { // у хвоста нет предыдущего элемента = если один элемент в списке
+                tail = null;
+                head = null;
+            } else {
+                tail = tail.previous;
+                tail.next = null;
+            }
+    }
+
+    boolean contains(int value){
+        Node current = head;
+        while (current != null){
+            if (current.value == value) return true;
+            current = current.next;
+        }
+        return false;
+    }
+
+    void BubbleSort(){
+        boolean ok;
+        do{
+            ok = false;
+            Node node = head;
+            while (node != null){
+                if(node.next!= null){
+                    if(node.value > node.next.value){
+                        ok = true;
+
+                        Node before = node.previous;
+                        Node current = node;
+                        Node next = node.next;
+                        Node after = next.next;
+
+                        current.previous = next;
+                        current.next = after;
+
+                        next.previous = before;
+                        next.next = current;
+
+                        if(before != null){
+                            before.next = next;
+                        } else {
+                            head = next;
+                        }
+                        if (after != null){
+                            after.previous = current;
+                        } else{
+                            tail = current;
+                        }
+
+                    }
+                }
+                node = node.next;
+            }
+        } while(ok);
+    }
+    void print(){
+        Node current = head;
+        while(current != null){
+            System.out.printf("%d ", current.value);
+            current = current.next;
+        }
+        System.out.println("");
+    }
+}
+public class SACODsem3 {
+    public static void main(String[] args) {
+        dList list = new dList();
+        for (int i = 10; i <=15 ; i++) {
+            list.push_front(i);
+        }
+        for (int i = 10; i > 5 ; i--) {
+            list.push_front(i);
+        }
+
+        list.print();
+        System.out.println("===============================");
+        list.BubbleSort();
+        list.print();
+
+    }
+}
--- src/Train.java
+++ src/Train.java
@@ -3,59 +3,53 @@ import java.io.*;
 import java.util.Arrays;
 
 public class Train {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = null;
 
-        try {
-            File file = new File("newFile.txt");
-            if(!file.exists())
-                file.createNewFile();
-
-            PrintWriter pw = new PrintWriter(file);
-            pw.println("12? + 4?5 = 591");
-            pw.println("everything is working fine");
-            pw.println("everything is working fine 2");
-            pw.close();
-
-            br = new BufferedReader(new FileReader("newFile.txt"));
-            String line;
-            line = br.readLine();
-            System.out.printf("Given the equation: %s\n", line);
-
-            String[] equation = line.split(" ");
-            String[] numbers = {equation[0],equation[2],equation[4]};
-            boolean flag = false;
-            for (int i = 0; i < 10; i++) {
-                int first = Integer.parseInt(numbers[0].replace("?", Integer.toString(i)));
-                for (int j = 0; j < 10; j++) {
-                    int second = Integer.parseInt(numbers[1].replace("?", Integer.toString(j)));
-                    if (first+second == Integer.parseInt(numbers[2])){
-                        numbers[0] = Integer.toString(first);
-                        numbers[1] = Integer.toString(second);
-                        flag = true;
-                    }
-                }
-            }
-            if (flag){
-                System.out.printf("Result: %s + %s = %s\n", numbers[0], numbers[1], numbers[2]);
-            } else{
-                System.out.println("No solution");
-            }
-
-//            System.out.printf("%s + %s = %s", numbers[0], numbers[1], numbers[2]);
-
-
-
-//            while((line = br.readLine()) != null){
-//                System.out.println(line);
-//
-
-        } catch(IOException e) {
-            System.out.print("Error: " + e);
-        } finally {
-            br.close();
-            System.out.println("pw Done");
+    public static void swap(int[] array, int first, int second){
+        int temp = array[first];
+        array[first] = array[second];
+        array[second] = temp;
+    }
+    public static void heapSort(int[] array){
+        for (int i = array.length/2-1; i >= 0 ; i--) {
+            heapify(array,i, array.length);
         }
+
+        for (int i = array.length-1; i >= 0; i--) {
+            swap(array, 0, i);
+            heapify(array,0,i);
+        }
+    }
+
+    public static void heapify(int[] array, int index, int size){
+        int leftChild = index*2 + 1;
+        int rightChild = index*2 +2;
+        if(rightChild < size && array[rightChild] > array[index]){
+            swap(array, rightChild, index);
+            heapify(array, rightChild, size);
+        }
+        if(leftChild < size && array[leftChild] > array[index]){
+            swap(array, leftChild, index);
+            heapify(array, leftChild, size);
+        }
+    }
+    public static void main(String[] args)  {
+        int[] array = {1,6,7,234,57,7,8,2,34,6,7,2345,1,3};
+        System.out.println("Origin array: ");
+        for (int item: array
+             ) {
+            System.out.print(item);
+            System.out.print(" ");
+        }
+        heapSort(array);
+        System.out.println("\nSorted array: ");
+        for (int item: array
+        ) {
+            System.out.print(item);
+            System.out.print(" ");
+        }
+
+        // Seminar3 - САКОД
+
 
     }
 }
