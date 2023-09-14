22697c6268dc7d2e2a87a52441f520de237f62a4
AndreiShimLeon <art.shim@yandex.ru>
2023-09-14 21:31:26 +0300

    hom4 done

--- src/Homework4.java
+++ src/Homework4.java
@@ -1,5 +1,37 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.*;
+
+
+class Calculator {
+    ArrayDeque<Integer> results;
+    public int calculate(char op, int a, int b) {
+        // Напишите свое решение ниже
+        int result = 0;
+        if(results == null) results = new ArrayDeque<>();
+        switch (op){
+            case '+':
+                result = a+b;
+                break;
+            case '-':
+                result = a-b;
+                break;
+            case '*':
+                result = a*b;
+                break;
+            case '/':
+                if (b != 0) {
+                    result = a/b;
+                } else {
+                    System.out.println("На ноль делить нельзя");
+                }
+                break;
+            case '<':
+                results.pollLast();
+                return results.pollLast();
+        }
+        results.add(result); // 10 6 7
+        return result;
+    }
+}
 class MyQueue<T>{
     //    В классе MyQueue реализуйте очередь с помощью LinkedList со следующими методами:
     LinkedList <T> queue;
@@ -87,6 +119,40 @@ public class Homework4 {
         System.out.println(queue.getElements());
 
         System.out.println(queue.first());
+        System.out.println("*~".repeat(100));
+
+
+        int a, b, c, d;
+        char op, op2, undo;
+
+        if (args.length == 0) {
+            // При отправке кода на Выполнение, вы можете варьировать эти параметры
+            a = 3;
+            op = '+';
+            b = 7;
+            c = 4;
+            op2 = '+';
+            d = 7;
+            undo = '<';
+        } else {
+            a = Integer.parseInt(args[0]);
+            op = args[1].charAt(0);
+            b = Integer.parseInt(args[2]);
+            c = Integer.parseInt(args[3]);
+            op2 = args[4].charAt(0);
+            d = Integer.parseInt(args[5]);
+            undo = args[6].charAt(0);
+        }
+
+        Calculator calculator = new Calculator();
+        int result = calculator.calculate(op, 5, 3);
+        System.out.println(result);
+        int result2 = calculator.calculate(op2, 3, 3);
+        System.out.println(result2);
+        int result3 = calculator.calculate(op2, 4, 3);
+        System.out.println(result3);
+        int prevResult = calculator.calculate(undo, 0, 0);
+        System.out.println(prevResult);
 
     }
 }
